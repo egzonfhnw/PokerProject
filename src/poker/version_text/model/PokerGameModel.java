db20d54653212bbd64b1666bb0796de9e25d481c
oliver <oliver.markovic@students.fhnw.ch>
2019-10-27 18:41:28 +0100

    Build a timeclock, which closes the program after 10 seconds

--- src/poker/version_text/model/PokerGameModel.java
+++ src/poker/version_text/model/PokerGameModel.java
@@ -1,6 +1,9 @@
 package poker.version_text.model;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.Scanner;
 
 import poker.version_text.PokerGame;
@@ -9,15 +12,25 @@ public class PokerGameModel {
 	private final ArrayList<Player> players = new ArrayList<>();
 	private DeckOfCards deck;
 	private int count;
+	Timer timer = new Timer();
+	TimerTask closeProgramm = new TimerTask() {
+	public void run() {
+	    System.exit(0);
+	    }
+	};
 	
 	public PokerGameModel() {
+	
 		Scanner userName = new Scanner(System.in);//Here we tried to add a scanner for changing the players name
 		System.out.println("Enter the number of players");
 		while(!userName.hasNextInt()) {
 
 			count++;
 			if(count == 3 ) {
-				System.out.println("self destruction in 3 seconds");
+				System.out.println("Programm will close itself due to three invalid inputs in 10 seconds");
+				timer.schedule(closeProgramm, new Date(System.currentTimeMillis()+10*1000));
+				
+				
 			
 			} else {
 				System.out.println("Input is not a number, try again");
